7632c362157d17a4853d41de534adcaea418b7be
Bibhu Prasad Sahoo <19btcse09@suiit.ac.in>
2024-05-27 01:13:08 +0530

    feat: Lab results ui complete

--- src/components/home/main-content/patient-details/details/Details.jsx
+++ src/components/home/main-content/patient-details/details/Details.jsx
@@ -5,10 +5,13 @@ import femaleIcon from '../../../../../assets/FemaleIcon.svg'
 import maleIcon from '../../../../../assets/MaleIcon.svg'
 import phoneIcon from '../../../../../assets/PhoneIcon.svg'
 import insuranceIcon from '../../../../../assets/InsuranceIcon.svg'
+import download from '../../../../../assets/download.svg'
 import { DUMMY_DATA } from '../../../../../dummyData'
 
 const Details = () => {
-    const patientDetails = DUMMY_DATA[0]
+    const patientDetails = DUMMY_DATA[0];
+    const labResultsList = DUMMY_DATA[0].lab_results;
+
     function formatDate(dateString) {
         // Create a Date object from the input string
         const dateParts = dateString.split('/');
@@ -30,60 +33,73 @@ const Details = () => {
     }
     const birthDate = formatDate(patientDetails.date_of_birth);
     return (
-        <div className={`${classes.details} card`}>
-            <div className={`${classes.profilePicture} flex-row`}>
-                <img src={patientDetails.profile_picture} alt="" />
+        <div className={`${classes.rightSection}`}>
+            <div className={`${classes.details} card`}>
+                <div className={`${classes.profilePicture} flex-row`}>
+                    <img src={patientDetails.profile_picture} alt="" />
+                </div>
+                <p className={classes.name}>{patientDetails.name}</p>
+                <div className={`${classes.info} flex-col`}>
+                    {/* Date of birth */}
+                    <div className={`${classes.infoItem} flex-row`}>
+                        <img src={birthIcon} alt="" />
+                        <div>
+                            <p className={classes.title}>Date of Birth</p>
+                            <p className={classes.value}>{birthDate}</p>
+                        </div>
+                    </div>
+
+                    {/* Gender */}
+                    <div className={`${classes.infoItem} flex-row`}>
+                        <img src={patientDetails.gender == 'Female' ? femaleIcon : maleIcon} alt="" />
+                        <div>
+                            <p className={classes.title}>Gender</p>
+                            <p className={classes.value}>{patientDetails.gender}</p>
+                        </div>
+                    </div>
+
+                    {/* Contact details */}
+                    <div className={`${classes.infoItem} flex-row`}>
+                        <img src={phoneIcon} alt="" />
+                        <div>
+                            <p className={classes.title}>Contact Info.</p>
+                            <p className={classes.value}>{patientDetails.phone_number}</p>
+                        </div>
+                    </div>
+
+                    {/* Emergency contact */}
+                    <div className={`${classes.infoItem} flex-row`}>
+                        <img src={phoneIcon} alt="" />
+                        <div>
+                            <p className={classes.title}>Emergency Contact</p>
+                            <p className={classes.value}>{patientDetails.emergency_contact}</p>
+                        </div>
+                    </div>
+
+                    {/* Insurance */}
+                    <div className={`${classes.infoItem} flex-row`}>
+                        <img src={insuranceIcon} alt="" />
+                        <div>
+                            <p className={classes.title}>Insurance Provider</p>
+                            <p className={classes.value}>{patientDetails.insurance_type}</p>
+                        </div>
+                    </div>
+
+                    {/* Show all button */}
+                    <div className={`${classes.showAll} flex-row`}>
+                        <button>Show All Details</button>
+                    </div>
+                </div>
             </div>
-            <p className={classes.name}>{patientDetails.name}</p>
-            <div className={`${classes.info} flex-col`}>
-                {/* Date of birth */}
-                <div className={`${classes.infoItem} flex-row`}>
-                    <img src={birthIcon} alt="" />
-                    <div>
-                        <p className={classes.title}>Date of Birth</p>
-                        <p className={classes.value}>{birthDate}</p>
-                    </div>
-                </div>
-
-                {/* Gender */}
-                <div className={`${classes.infoItem} flex-row`}>
-                    <img src={patientDetails.gender == 'Female' ? femaleIcon : maleIcon} alt="" />
-                    <div>
-                        <p className={classes.title}>Gender</p>
-                        <p className={classes.value}>{patientDetails.gender}</p>
-                    </div>
-                </div>
-
-                {/* Contact details */}
-                <div className={`${classes.infoItem} flex-row`}>
-                    <img src={phoneIcon} alt="" />
-                    <div>
-                        <p className={classes.title}>Contact Info.</p>
-                        <p className={classes.value}>{patientDetails.phone_number}</p>
-                    </div>
-                </div>
-
-                {/* Emergency contact */}
-                <div className={`${classes.infoItem} flex-row`}>
-                    <img src={phoneIcon} alt="" />
-                    <div>
-                        <p className={classes.title}>Emergency Contact</p>
-                        <p className={classes.value}>{patientDetails.emergency_contact}</p>
-                    </div>
-                </div>
-
-                {/* Insurance */}
-                <div className={`${classes.infoItem} flex-row`}>
-                    <img src={insuranceIcon} alt="" />
-                    <div>
-                        <p className={classes.title}>Insurance Provider</p>
-                        <p className={classes.value}>{patientDetails.insurance_type}</p>
-                    </div>
-                </div>
-
-                {/* Show all button */}
-                <div className={`${classes.showAll} flex-row`}>
-                    <button>Show All Details</button>
+            <div className={`${classes.labResults} card`}>
+                <p className={classes.header}>Lab Results</p>
+                <div className={`${classes.resultsList} flex-col`}>
+                    {labResultsList.map(item =>
+                        <div className={`${classes.resultElement} flex-row`}>
+                            <p>{item}</p>
+                            <img src={download} alt="" />
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
